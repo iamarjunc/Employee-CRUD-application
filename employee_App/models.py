6f7824bd8fbd4ce3dac29bca5006216ed6d83abe
Arjun c <arjunchandrakumar2017@gmail.com>
2023-09-30 20:17:51 +0530

    first commit

---
+++ employee_App/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    photo = models.ImageField(upload_to='employee_photos/', null=True, blank=True)
